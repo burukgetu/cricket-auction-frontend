4eaec02445818f03b93a8576b5ddd6c538e3144e
burukgetu <burukgetu000@gmail.com>
2024-11-17 00:09:46 -0300

    added verce json

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
